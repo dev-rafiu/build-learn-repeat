a55a4b618c1801b589d2e8c46d147aeb2e13d09f
Rafiw Idrissu <dev.rafiu@gmail.com>
2024-05-26 18:03:02 -0700

    server file update

--- jobify/server/server.js
+++ jobify/server/server.js
@@ -6,14 +6,15 @@ import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandleMiddleware from "./middleware/error-handler.js";
 
 app.get("/", (req, res) => {
-  throw new Error("error");
+  // throw new Error("error");
   res.send("Welcome home !!");
 });
 
 app.use(notFoundMiddleware);
 app.use(errorHandleMiddleware);
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
+  console.log(`server is listening on port ${PORT}`);
 });
