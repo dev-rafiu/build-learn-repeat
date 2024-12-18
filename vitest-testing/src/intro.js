2f2c923034482cc8ebe9107e20b5d80c035fae30
Rafiw Idrissu <dev.rafiu@gmail.com>
2024-12-18 09:02:49 +0000

    feat: vitest testing

---
+++ vitest-testing/src/intro.js
@@ -0,0 +1,19 @@
+// Lesson: Writing your first tests
+export function max(a, b) {
+  return a > b ? a : b;
+}
+
+// Exercise
+export function fizzBuzz(n) {
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+  if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
+  return n.toString();
+}
+
+// Exercise
+export function factorial(n) {
+  if (n == 0 || n == 1) return 1;
+
+  return n * factorial(n - 1);
+}
---
+++ vitest-testing/test/intro.test.js
@@ -0,0 +1,43 @@
+import { describe, test, it, expect } from "vitest";
+
+import { max, fizzBuzz, factorial } from "../src/intro";
+
+// "describe" is used to create a test suite or a group of related tests
+
+describe("max", () => {
+  it("returns the first arg if it is greater", () => {
+    expect(max(2, 1)).toBe(2);
+  });
+
+  it("returns the second arg if it is greater", () => {
+    expect(max(2, 5)).toBe(5);
+  });
+
+  it("returns the first arg if args are equal", () => {
+    expect(max(2, 2)).toBe(2);
+  });
+});
+
+describe("fizzBuzz", () => {
+  it("returns FizzBuzz if arg is divisible by 3 & 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  it("returns Fizz if arg is divisible by 3 only", () => {
+    expect(fizzBuzz(9)).toBe("Fizz");
+  });
+
+  it("returns buzz if arg is divisible by 5 only", () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
+
+  it("returns string arg if arg is not divisible by 3 & 5", () => {
+    expect(fizzBuzz(8)).toBe("8");
+  });
+});
+
+describe("factorial", () => {
+  it("returns the factorial of a number", () => {
+    expect(factorial(4)).toBe(24);
+  });
+});
