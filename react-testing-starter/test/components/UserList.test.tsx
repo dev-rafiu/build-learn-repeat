c3234bf166185a2b45a8e2ceeb47968c866d0899
Rafiw Idrissu <dev.rafiu@gmail.com>
2025-01-14 16:29:23 +0000

    feat: test UserList

--- react-testing-starter/test/components/UserList.test.tsx
+++ react-testing-starter/test/components/UserList.test.tsx
@@ -1,8 +1,26 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import UserList from "../../src/components/UserList";
+import { User } from "../../src/entities";
 
-describe("group", () => {
+describe("UserList", () => {
   it("should render the text no users if there are no users", () => {
-    render(<UserList users={[]} />);
+    const users: User[] = [];
+    render(<UserList users={users} />);
+
+    expect(screen.getByText(/no users/i)).toBeInTheDocument();
+  });
+
+  it("should render a list of users if there are users", () => {
+    const users: User[] = [
+      { id: 1, name: "dev" },
+      { id: 2, name: "abd" },
+    ];
+    render(<UserList users={users} />);
+
+    users.forEach((user) => {
+      const link = screen.getByRole("link", { name: user.name });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute("href", `/users/${user.id}`);
+    });
   });
 });
