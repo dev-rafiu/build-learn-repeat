68f15136cfe5749a99cbf00f9bf646a4c92d7ae2
Rafiw Idrissu <dev.rafiu@gmail.com>
2025-09-29 12:58:36 +0000

    chore: delete doordash loader dir

--- doordash-loader/src/app/fetch-data.tsx
+++
@@ -1,6 +0,0 @@
-export const fetchPageData = async (page: string) => {
-  console.log("fpd");
-
-  await new Promise((res) => setTimeout(res, 3000));
-  return { content: `Content for ${page}` };
-};
--- doordash-loader/src/components/Skeleton.tsx
+++
@@ -1,8 +0,0 @@
-// components/Skeleton.js
-import React from "react";
-
-const Skeleton = () => (
-  <div className="absolute inset-0 bg-black/50 animate-pulse" />
-);
-
-export default Skeleton;
